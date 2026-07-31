119d7ff4b8469afe425fbd6b3de4c5ce8e5983d4
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2026-07-31 11:34:52 -0300

    merge main, removed local implementation onFocusReceived
    feat: implement DeltaTextInputService to handle IME text deltas with offset adjustment

--- lib/src/editor/editor_component/service/ime/delta_input_service.dart
+++ lib/src/editor/editor_component/service/ime/delta_input_service.dart
@@ -186,10 +186,6 @@ class DeltaTextInputService extends TextInputService with DeltaTextInputClient {
       }
     }
   }
-
-  //new callback on flutter 3.44
-  @override
-  bool onFocusReceived() => attached;
 }
 
 const String _whitespace = ' ';
